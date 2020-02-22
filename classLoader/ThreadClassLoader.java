fe4c5cab86dbce193e40ceed02b39a4657f840d4
shitsurei <lfgewj21345@163.com>
2020-02-22 11:06:05 +0800

    线程上下文类加载器

--- classLoader/ThreadClassLoader.java
+++ classLoader/ThreadClassLoader.java
@@ -14,21 +14,38 @@ import classLoader.samples.MyThread;
  * 【如果没有用setContextClassLoader方法设置当前线程的上下文类加载，则会自动继承父线程的上下文类加载器】
  * Java应用运行时的初始线程的上下文类加载器是系统类加载器
  *
- * SPI（Service Provider Interface）：JNDI、JDBC等等
+ * SPI（Service Provider Interface）：JNDI、JDBC、JAXP等等
  * 以JDBC为例，JDBC只是一种标准，不同数据库的厂商有其各自的实现，我们使用时只需要将厂商提供的jar包放入类路径下即可
  * 问题在于JDBC的接口相关代码位于rt.jar包下，是由根类加载器加载的，而具体实现的代码是系统类加载器加载的，如果遵循双亲委派机制根类加载器加载的类就无法访问子类加载器所加载的类
  * 在双亲委托模型下，类加载是由下至上的，即下层的类加载器会委托上层加载；
  * 但对于SPI来说有些接口是Java核心库所提供的，而Java核心库是由启动类加载器来加载的，而这些接口的实现却来自于不同的jar包（不同厂商提供）
  * Java的启动类加载器不会加载其他来源的jar包，这样传统的双亲委派模型就无法满足SPI的要求。
  * 【类加载器的双亲可以使用当前线程所指定的上下文类加载器所加载的类，这就改变了父类加载器不能使用子类加载器或】
+ *
+ * 当高层提供了统一的接口让低层去实现，同时又要在高层加载（或实例化）底层的类时，就必须要通过线程上下文类加载器来帮助高层的classLoader找到并加载该类
+ * 线程上下文类加载器的一般使用模式：获取-使用-还原（伪代码如下）
+ * ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+ * try {
+ *     Thread.currentThread().setContextClassLoader(targetClassLoader);
+ *     该方法中会使用到targetClassLoader，通过Thread.currentThread().getContextClassLoader();语句调用
+ *     myMethod();
+ * }finally {
+ *     Thread.currentThread().setContextClassLoader(classLoader);
+ * }
  */
 public class ThreadClassLoader {
     public static void main(String[] args) {
+//        unChanged();
+    }
+
+    /**
+     * 测试未修改当前线程类加载器
+     */
+    public static void unChanged(){
 //        sun.misc.Launcher$AppClassLoader@18b4aac2
         System.out.println(Thread.currentThread().getContextClassLoader());
 //        null  Thread为lang包中的类，由根类加载器加载
         System.out.println(Thread.class.getClassLoader());
-
         new MyThread();
     }
 }
